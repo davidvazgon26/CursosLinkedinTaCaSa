15e5d3b367c4e6ceca53835cd58a33f7c0fc7f42
davidvazgon26 <davidvazgon@hotmail.com>
2023-01-19 23:54:34 -0600

    iniciando ejercicios con useState

--- EjerciciosReact/ReactHooks/reacthooksejemplos/src/App.js
+++ EjerciciosReact/ReactHooks/reacthooksejemplos/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 
-function App() {
+function App({name}) {
   return (
     <div className="App">
       <h1>Hola Hooks</h1>
+      <h3>Bienvenido a tu curso de hooks {name}</h3>
     </div>
   );
 }
